6cdd63bfbb047dfde43e25b25c26a4610e6c1ca5
naranjwara <nabilahnajwa808@gmail.com>
2026-06-03 20:17:20 +0700

    Update Frontend API URL

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,7 +1,7 @@
-import { API_BASE_URL } from '../config/api'
+import { API_URL } from '../config/api'
 
 export const apiClient = async (path, options = {}) => {
-  const response = await fetch(`${API_BASE_URL}${path}`, {
+  const response = await fetch(`${API_URL}${path}`, {
     headers: {
       'Content-Type': 'application/json',
       ...options.headers,
