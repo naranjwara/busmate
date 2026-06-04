57bf068a32c345b4ce9d167e952ed1f61571454d
naranjwara <nabilahnajwa808@gmail.com>
2026-06-04 14:54:39 +0700

    Add railway as allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['busmate-production-72ae.up.railway.app', '.up.railway.app'],
+  },
 })
